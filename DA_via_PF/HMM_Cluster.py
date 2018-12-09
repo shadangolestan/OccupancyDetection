96dcae25e47474d36b38d2226c80d64896ab9c86
Abdullah-Al-Zishan <azishan@ualberta.ca>
2018-12-09 14:55:39 -0700

    supervised learn

--- DA_via_PF/HMM_Cluster.py
+++ DA_via_PF/HMM_Cluster.py
@@ -1,5 +1,6 @@
 import numpy as np
 import data_analyzer as da
+import scipy.stats as stats
 
 class HMM_Cluster():
     def __init__(self, time_intervals=None, occu_intervals=None, emi_intervals=None):
@@ -48,6 +49,16 @@ class HMM_Cluster():
                 return i
         return -1
         
+    def emi_prob(self, hmm_, hidd_, emi_, type_=None):
+        if type_ == None:
+            return self.B[hmm_][hidd_][sefl.get_emi_index(emi)]
+        if type_ == 'gamma':
+            fit_alpha = self.gamma_params[hmm_][hidd_][0]
+            fit_loc = self.gamma_params[hmm_][hidd_][1]
+            fit_beta = self.gamma_params[hmm_][hidd_][2]
+            return stats.gamma.pdf(x=emi, a=fit_alpha, loc=fit_loc, scale=fit_beta)
+        return 0.0
+        
     def supervised_learn(self, data, seq, prior=None):
         if prior != None:
             self.A_count = np.copy(prior[0])
@@ -58,9 +69,81 @@ class HMM_Cluster():
         data_points = []
         for i in range(self.number_of_hmms):
             x = []
-            for s in seq:
-                for j in range(s[0], s[1]):
-                    if self.get_hmm_index(data[j][da.Hour]) == i:
-                        x.append(j)
+            prev = -10
+            y = []
+            for j in range(seq[0], seq[1]):
+                hmm_ = self.get_hmm_index(data[j][da.Hour])
+                if hmm_ == i and prev != i:
+                    y = [j]
+                if hmm_ == i and prev == i:
+                    y.append(j)
+                if hmm_ != i and prev == i:
+                    x.append(y)
+                    y = []
+                prev = hmm_
+            if len(y) > 0:
+                x.append(y)
             data_points.append(x)
-            
+
+
+        #############################Learning A####################
+        for i in range(self.number_of_hmms):
+            for s in data_points[i]:
+                for j in s[:-1]:
+                    from_ = self.get_occu_index(data[j][da.OCC])
+                    to_ = self.get_occu_index(data[j+1][da.OCC])
+                    self.A_count[i][from_][to_] += 1
+            for j in range(self.number_of_hidd_states):
+                sum_ = 1.*np.sum(self.A_count[i][j])
+                self.A[i][j] = self.A_count[i][j]/sum_
+        ###########################################################
+
+        #############################Learning B####################
+        for i in range(self.number_of_hmms):
+            for s in data_points[i]:
+                for j in s[:-1]:
+                    emi_ = self.get_emi_index(data[j][da.EMI])
+                    from_ = self.get_occu_index(data[j][da.OCC])
+                    self.B_count[i][from_][emi_] += 1
+            for j in range(self.number_of_hidd_states):
+                sum_ = 1.*np.sum(self.B_count[i][j])
+                self.B[i][j] = self.B_count[i][j]/sum_
+        ###########################################################
+
+        #############################Learning B Gamma##############
+        self.gamma_params = np.zeros((self.number_of_hmms, self.number_of_hidd_states, 3))
+        gamma_ = []
+        for i in range(self.number_of_hmms):
+            x = [] 
+            for j in range(self.number_of_hidd_states):
+                y = [0.0, 0.5]
+                for s in data_points[i]:
+                    for k in s:
+                        hidd_ = self.get_occu_index(data[k][da.OCC])
+                        if hidd_ == j:
+                            y.append(data[i][da.EMI])
+                x.append(y)
+            gamma_.append(x)
+        
+        for i in range(self.number_of_hmms):
+            for j in range(self.number_of_hidd_states):
+                fit_alpha, fit_loc, fit_beta = stats.gamma.fit(gamma_[i][j])
+                self.gamma_params[i][j][0] = fit_alpha
+                self.gamma_params[i][j][1] = fit_loc
+                self.gamma_params[i][j][2] = fit_beta
+        ###########################################################
+
+        #############################Learning PI####################
+        for i in range(self.number_of_hmms):
+            for s in data_points[i]:
+                hidd_ = self.get_occu_index(data[s[0]][da.OCC])
+                self.PI_count[i][hidd_] += 1
+            sum_ = 1.*np.sum(self.PI_count[i])
+            self.PI[i] = self.PI_count[i]/sum_
+        ###########################################################
+
+        print(self.gamma_params)
+        print(self.PI_count)
+
+HMM = HMM_Cluster()
+HMM.supervised_learn(da.room_data, seq=[0, 2880])
